f6ac825722c45adf2f8d038906e52222467dab79
Leonardo <leonardobazzana333@yahoo.com>
2025-07-22 08:18:36 -0300

    Alterações Finais no main.py

--- main.py
+++ main.py
@@ -17,33 +17,34 @@ def main():
         try:
             opcao = menu()
 
-            if opcao == "1":
-                nome = input("Nome: ")
-                sobrenome = input("Sobrenome: ")
-                email = input("Email: ")
-                numero = input("Número: ")
-                agenda.adiciona_pessoa(nome, sobrenome, email, numero)
+            match opcao:
+                case 1:
+                    nome = input("Nome: ")
+                    sobrenome = input("Sobrenome: ")
+                    email = input("Email: ")
+                    numero = input("Número: ")
+                    agenda.adiciona_pessoa(nome, sobrenome, email, numero)
 
-            elif opcao == "2":
-                termo = input("Digite o nome ou sobrenome para buscar: ")
-                agenda.buscar_pessoa(termo)
+                case 2:
+                    termo = input("Digite o nome ou sobrenome para buscar: ")
+                    agenda.buscar_pessoa(termo)
 
-            elif opcao == "3":
-                agenda.mostrar_registro()
+                case 3:
+                    agenda.mostrar_registro()
 
-            elif opcao == "4":
-                try:
-                    id_remover = int(input("Digite o ID do contato a remover: "))
-                    agenda.remover_pessoa(id_remover)
-                except ValueError:
-                    print("ID inválido.")
+                case 4:
+                    try:
+                        id_remover = int(input("Digite o ID do contato a remover: "))
+                        agenda.remover_pessoa(id_remover)
+                    except ValueError:
+                        print("ID inválido.")
 
-            elif opcao == "5":
-                print("Encerrando a execução.")
-                break
+                case 5:
+                    print("Encerrando a execução.")
+                    break
 
-            else:
-                print("Opção inválida. Tente novamente.")
+                case _:
+                    print("Opção inválida. Tente novamente.")
 
         except Exception as e:
             print(f"Erro durante a execução: {e}")
